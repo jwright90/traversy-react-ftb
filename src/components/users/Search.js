194205e8f09831cd01249e0c9229649211475ad7
jwright90 <jason.wright90@gmail.com>
2021-09-09 20:07:03 +0100

    add clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   }
 
   onChange = e => {
@@ -31,6 +33,9 @@ class Search extends Component {
           <input type="submit" value="Search"
             className="btn btn-dark btn-block" />
         </form>
+        {this.props.showClear &&
+          <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
+        }
       </div>
     )
   }
