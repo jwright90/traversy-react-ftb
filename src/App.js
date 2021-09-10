90ece36af810250b66d7c6d5210a117873f91417
jwright90 <jason.wright90@gmail.com>
2021-09-10 18:52:53 +0100

    add user pages and repos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import About from './components/pages/About';
 import Alert from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import Search from './components/users/Search';
+import User from './components/users/User';
+import Users from './components/users/Users';
 import axios from 'axios';
 import './App.css';
+
 
 class App extends Component {
 
   state = {
     users: [],
+    user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   }
 
   // async componentDidMount() {
@@ -28,6 +34,28 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // Get single GitHub user
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios
+      .get(`https://api.github.com/users/${username}
+        ?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({ user: res.data, loading: false });
+  }
+
+  // Get users repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios
+      .get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc
+        &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({ repos: res.data, loading: false });
+  }
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -39,21 +67,41 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
 
-          <Users loading={this.state.loading} users={this.state.users} />
+    const { users, user, loading, repos } = this.state;
+
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={this.state.users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading} />
+              )} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,49 +1,46 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-  state = {
-    text: ''
+const Search = ({ searchUsers, clearUsers, setAlert, showClear }) => {
+
+  const [text, setText] = useState('');
+
+  const onChange = e => {
+    setText(e.target.value);
   }
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  }
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === '') {
-      this.props.setAlert('Please enter something...', 'light');
+    if ({ text } === '') {
+      setAlert('Please enter something...', 'light');
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: '' });
+      searchUsers(text);
+      setText('');
     }
   }
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input type="text" name="text"
+          value={text}
+          onChange={onChange}
+          placeholder="Search Users..." />
+        <input type="submit" value="Search"
+          className="btn btn-dark btn-block" />
+      </form>
+      {showClear &&
+        <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+      }
+    </div>
+  )
+}
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input type="text" name="text"
-            value={this.state.text}
-            onChange={this.onChange}
-            placeholder="Search Users..." />
-          <input type="submit" value="Search"
-            className="btn btn-dark btn-block" />
-        </form>
-        {this.props.showClear &&
-          <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
-        }
-      </div>
-    )
-  }
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
